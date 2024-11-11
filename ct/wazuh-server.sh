a1f8e27a44daaa9ae41ff75836fc0933fbffa016
Adrien RICHARD <richardadrien0@gmail.com>
2024-11-11 16:35:28 +0100

    cahned name

--- ct/wazuh-server.sh
+++ ct/wazuh-server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/community-scripts/Mrgove10/main/misc/build.func)
 # Copyright (c) 2024 Adrien R.
 # Author: Adrien R.
 # License: MIT
@@ -17,7 +17,7 @@ EOF
 }
 header_info
 echo -e "Loading..."
-APP="Wazuh"
+APP="Wazuh-server"
 var_disk="5"
 var_cpu="2"
 var_ram="1024"
